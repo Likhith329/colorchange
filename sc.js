0cd9fffe418d1a0227345b1432e0baca619cf5cb
Likhith329 <likhithmattapalli@gmail.com>
2022-09-02 11:26:07 +0530

    first commit

---
+++ sc.js
@@ -0,0 +1,16 @@
+var a=0
+var tdat=document.getElementById("txt")
+function f1(){
+a++
+tdat.innerHTML=a
+}
+
+function f2(){
+    a--
+    tdat.innerHTML=a
+}
+
+function f3(){
+    a=0
+    tdat.innerHTML=a
+}
